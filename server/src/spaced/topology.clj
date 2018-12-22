82d0f9012f3e4f24c466e782db87b1f9c3922a4b
Tristan Straub <tristanstraub@gmail.com>
2018-12-23 10:59:44 +1100

    Custom store

--- server/src/spaced/topology.clj
+++ server/src/spaced/topology.clj
@@ -1,15 +1,42 @@
 (ns spaced.topology
-  (:require [spaced.transit-serdes])
+  (:require spaced.transit-serdes)
   (:import java.util.Properties
            [org.apache.kafka.clients.admin AdminClient AdminClientConfig NewTopic]
-           org.apache.kafka.clients.consumer.KafkaConsumer
            [org.apache.kafka.clients.producer KafkaProducer ProducerRecord]
-           [org.apache.kafka.common.serialization Serdes Serdes$LongSerde Serdes$StringSerde StringDeserializer StringSerializer]
-           [org.apache.kafka.streams KeyValue KafkaStreams StreamsBuilder StreamsConfig TopologyDescription$Processor TopologyDescription$Sink TopologyDescription$Source TopologyTestDriver]
-           [org.apache.kafka.streams.kstream Transformer Reducer TransformerSupplier ValueTransformerSupplier]
-           org.apache.kafka.streams.state.Stores
-           org.apache.kafka.streams.test.ConsumerRecordFactory
-           (org.apache.kafka.streams.processor ProcessorSupplier Processor)))
+           [org.apache.kafka.streams KafkaStreams StreamsBuilder StreamsConfig TopologyTestDriver]
+           (org.apache.kafka.streams.kstream Transformer TransformerSupplier)
+           [org.apache.kafka.streams.processor StateRestoreCallback StateStore]
+           org.apache.kafka.streams.state.StoreBuilder
+           org.apache.kafka.streams.test.ConsumerRecordFactory))
+
+(defrecord MyStateStore [name context open records]
+  StateStore
+  (init [this ctx root]
+    (reset! context ctx)
+    (.register ctx root
+               (reify StateRestoreCallback
+                 (restore [this key value]
+;;                   (println :restore key value)
+                   ;;                       (.put this )
+                   )
+                 ))
+    (reset! open true))
+  (flush [_])
+  (close [_])
+  (isOpen [_]
+    @open)
+  (name [_]
+    name)
+  (persistent [_]
+    true))
+
+(defn state-store
+  [name]
+  (let [context (atom nil)
+        open    (atom nil)]
+    (->MyStateStore name context open (atom []))))
+
+;;(state-store {:keys ["kd-tree-1"]})
 
 (defn setup!
   []
@@ -55,23 +82,62 @@
   [topology]
   (KafkaStreams. topology (stream-props)))
 
+(defn store-put-item!
+  [store item]
+  (swap! (:records store) conj item))
+
 (defn topology
   []
-  (let [builder (StreamsBuilder.)]
+  (let [builder (StreamsBuilder.)
+        logging (atom false)]
+
+    ;; (.addStateStore builder
+    ;;                 (Stores/keyValueStoreBuilder
+    ;;                  (Stores/persistentKeyValueStore "test2")
+    ;;                  (Serdes$StringSerde.)
+    ;;                  (Serdes$LongSerde.)))
+    
+    (.addStateStore builder
+                    (reify StoreBuilder
+                      (build [_]
+                        (state-store "Test"))
+                      (loggingEnabled [this]
+                        @logging)
+                      (withCachingEnabled [this]
+                        (throw (ex-info "Not supported" {})))
+                      (withLoggingEnabled [this _]
+                        (reset! logging true)
+                        this)
+                      (withLoggingDisabled [this]
+                        (reset! logging false)
+                        this)
+                      (name [_]
+                        "Test")))
+    
     (-> builder
         (.stream "objects")
-        (.join (reify AggregatorSupplier
+        (.transform (reify TransformerSupplier
                       (get [this]
-                        (reify Processor
-                          (init [this ctx])
-                          (close [this])
-                          (process [this k v]
-                            nil)))))
+                        (let [context (atom nil)
+                              store   (atom nil)]
+                          (reify Transformer
+                            (init [this ctx]
+                              (reset! store (.getStateStore ctx "Test"))
+                              (reset! context ctx))
+                            (close [this])
+                            (transform [this k v]
+                              (store-put-item! @store [0 0])
+                              (.forward @context "key" [0 0]))))))
+                    (into-array String ["Test"]))
+
+        (.groupByKey)
         (.reduce (reify Reducer
                    (apply [this agg val]
                      (str agg ":" val))))
         (.toStream)
-        (.through "output-topic"))
+        (.through "output-topic")
+        )
+
     (.build builder)))
 
 (with-open [driver (TopologyTestDriver. (topology) (stream-props))]
@@ -79,6 +145,15 @@
                                         (spaced.transit-serdes/transit-serializer*)
                                         (spaced.transit-serdes/transit-serializer*))]
     (.. driver (pipeInput (.. records (create "objects" "key" {:apples "value"}))))
+    (.. driver (pipeInput (.. records (create "objects" "key" {:apples "value"}))))
+    (.. driver (pipeInput (.. records (create "objects" "key" {:apples "value"}))))
+    (.. driver (pipeInput (.. records (create "objects" "key" {:apples "value"}))))
+    (println :test @(:records (.getStateStore driver "Test")))
     (.. driver (readOutput "output-topic"
                            (spaced.transit-serdes/transit-deserializer*)
-                           (spaced.transit-serdes/transit-deserializer*)))))
+                           (spaced.transit-serdes/transit-deserializer*)))
+
+    (.. driver (readOutput "output-topic"
+                           (spaced.transit-serdes/transit-deserializer*)
+                           (spaced.transit-serdes/transit-deserializer*)))    
+    ))
