0549da54eae6d1d303c7c450437d2c1ec88c4f4b
Tristan Straub <tristanstraub@gmail.com>
2018-12-30 10:57:21 +1100

    Remove server dups

--- client/src/spaced/distance.clj
+++
@@ -1,9 +0,0 @@
-(ns spaced.distance
-  (:require [kdtree :as kdtree]))
-
-(def points [[8 8] [3 1] [6 6] (with-meta [7 7] {:object 'x}) [1 3] [4 4] [5 5]])
-;; Build a kdtree from a set of points
-(def tree (kdtree/build-tree points))
-(println "Tree:" tree)
-
-(meta (second (kdtree/nearest-neighbor tree [8 8] 2)))
